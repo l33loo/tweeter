7b4d0472cd857d02b43aaad6bc15a862d9abda8a
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-03-28 03:18:01 +0000

    Add basic functionality to tweet character count

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
+  $(".new-tweet textarea").on("keypress", function(event) {
+    var usedChar = ($(this).val()).length + 1;
+    var charLeft = 140 - usedChar;
 
+    $("span.counter").text(140 - usedChar);
+  });
 });
