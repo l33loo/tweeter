645db911ebc57bc5cdd1e303032feb28d1b028cc
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-03-28 18:46:37 +0000

    Add hover effet for posted tweets, and other minor edits

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,20 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Hightlight tweet when hover over it
+$(document).ready(function() {
+  $("article.tweet").hover(
+    function() {
+      $(this).attr("id", "hover");
+      $("article.tweet footer").attr("id", "hover-footer");
+    }, function() {
+      $(this).attr("id", null);
+      $("article.tweet footer").attr("id", null);
+    }
+
+    //   $(this).addClass("hover");
+    // }, function() {
+    //   $(this).removeClass("hover");
+    // }
+  );
+});
--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
-  $(".new-tweet textarea").on("keypress", function(event) {
-    var usedChar = ($(this).val()).length + 1;
-    var charLeft = 140 - usedChar;
-
-    $("span.counter").text(140 - usedChar);
+  $(".new-tweet textarea").on("keydown", function(event) {
+    var counter = $(this).siblings(".counter");
+    var charLeft = 140 - ($(this).val().length + 1);
+    if (charLeft < 0) {
+      counter.addClass("over-char-limit");
+    } else {
+      counter.removeClass("over-char-limit");
+    }
+    counter.text(charLeft);
   });
 });
+
+// dsfadsfadsfasdfasdfasdfasdfadsfasdfasdfasdfadsfasdfsadfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfffffffasdfadsfasdfsdfasdfasdfasdfasdfasdfasdfsdf
