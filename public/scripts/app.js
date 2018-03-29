1bf524475bc0e435dec447d6bb293fa7f9f83ae3
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-03-29 05:42:53 +0000

    Add Ajax requests to render all tweets upon new tweet submission, and delete hard coded tweet database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,74 +6,7 @@
 
 
 
-
-// TEST addition of new tweet
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
-function renderTweets(tweets) {
-  // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-}
-
 function createTweetElement(tweet) {
-  let $tweet = $('<article>').addClass('tweet');
-  // ...
-  return $tweet;
-}
-
-renderTweets(data);
-
-
-function createTweetElement(tweet) {
-  // console.log("YAY");
-  // console.log(tweet);
   var $tweet = $("<article>").addClass("tweet").append(`
     <header>
       <img src="${tweet.user.avatars.small}" alt="Avatar"/>
@@ -82,10 +15,9 @@ function createTweetElement(tweet) {
     </header>
       <p>${tweet.content.text}</p>
       <footer>
-        ${tweet.created_at}
+        Posted ${tweet.created_at}
       </footer>
     `);
-  // console.log($tweet);
   return $tweet;
 }
 
@@ -114,6 +46,16 @@ $(document).ready(function() {
     $(this).find("span.hover").remove();
   });
 
-  // Create new tweet
-  renderTweets(data);
+  var tweet = $(".new-tweet form");
+  $(tweet).on('submit', function(event) {
+    event.preventDefault();
+    console.log('Button clicked, performing ajax call...');
+    let data = $(tweet).serialize();
+    $.post("/tweets", data).done(function() {
+      $.get("/tweets").done(function(tweetsArr){
+        renderTweets(tweetsArr);
+        $("textarea").val("");
+      });
+    });
+  });
 });
