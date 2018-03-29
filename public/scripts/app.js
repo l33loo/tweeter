778d7929acb6e4426d9de5cb4a4bd124e6b369ff
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-03-29 06:01:28 +0000

    Add how long ago (in days or years) each tweeter post was submitted

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,27 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+function daysAgo(date) {
 
+  // Calculate the number of days from milliseconds
+  let msecInADay = 1000 * 60 * 60 * 24;
+
+  let dayElapsed = Math.round((Date.now() - date) / msecInADay);
+  if (dayElapsed >= 365) {
+    let years = Math.round(dayElapsed / 365);
+    if (years === 1) {
+      return "a year ago";
+    } else {
+      return `${years} years ago`;
+    }
+  } else if (dayElapsed > 1) {
+    return `${dayElapsed} days ago`;
+  } else if (dayElapsed === 1) {
+    return "yesterday";
+  } else if (!dayElapsed) {
+    return "today";
+  }
+}
 
 function createTweetElement(tweet) {
   var $tweet = $("<article>").addClass("tweet").append(`
@@ -15,7 +35,7 @@ function createTweetElement(tweet) {
     </header>
       <p>${tweet.content.text}</p>
       <footer>
-        Posted ${tweet.created_at}
+        Posted ${daysAgo(tweet.created_at)}
       </footer>
     `);
   return $tweet;
